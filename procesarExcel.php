76f34e13137a196b2371157b3c78ae2b64ea0857
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-11-25 12:59:30 +0100

    Carga masiva de eventos, validacion y respuesta

--- procesarExcel.php
+++ procesarExcel.php
@@ -14,8 +14,13 @@ if ($conn->connect_error) {
 
 if( fopen("C:\wamp64\www\carpeta\EventosDeportivosCrud\dbExcel.csv","r")) { 
   $file = fopen("C:\wamp64\www\carpeta\EventosDeportivosCrud\dbExcel.csv","r");
+	$contador = 0;
+	$contadorExito = 0;
+	$errores = [];
 
   while(($data = fgetcsv($file, 1000, ","))) {
+		$filaErrores = [];
+		$contador ++;
 
 		$evento = $conn->real_escape_string($data[0]);
 		$tipoDeporte = $conn->real_escape_string($data[1]);
@@ -23,6 +28,46 @@ if( fopen("C:\wamp64\www\carpeta\EventosDeportivosCrud\dbExcel.csv","r")) {
 		$hora = $conn->real_escape_string($data[3]);
 		$ubicacion = $conn->real_escape_string($data[4]);
 		$organizador = $conn->real_escape_string($data[5]);
+
+		if (empty($evento)) { 
+			$filaErrores['evento'][] = 'Es obligatorio el campo del Evento'; 
+		} else if (strlen($evento) <= 3 && strlen($evento) >= 15) {
+			$filaErrores['evento'][] = 'Tiene que ser entre 3 y 15 caracteres'; 
+		}
+
+		if (empty($tipoDeporte)) { 
+			$filaErrores['tipo_deporte'][] = 'Es obligatorio el campo del Tipo de Deporte'; 
+		} else if (!in_array($tipoDeporte, ['futbol', 'baloncesto', 'tenis'])) { 
+			$filaErrores['tipo_deporte'][] = 'Solo se permite los valores futbol, baloncesto, tenis'; 
+		}
+
+		if (empty($fecha)) { 
+			$filaErrores['fecha'][] = 'La fecha es un campo requerido'; 
+		} else if (!preg_match("/\d{2}-\d{2}-\d{4}/", $fecha)) { 
+			$filaErrores['fecha'][] = 'El formato de la fecha no es valido, DD-MM-YYYY'; 
+		}
+
+		if (empty($hora)) { 
+			$filaErrores['hora'][] = 'La hora es requerida'; 
+		} else if (!preg_match("/\d{2}:\d{2}/", $hora)) { 
+			$filaErrores['hora'][] = 'El formato de la hora no es valida'; 
+		}
+
+		if (empty($ubicacion)) { 
+			$filaErrores['ubicacion'][] = 'La Ubicacion es requerida'; 
+		} else if (strlen($ubicacion) > 255) { 
+			$filaErrores['ubicacion'][] = 'La Ubicacion no puede superar los 255 caracteres'; 
+		} 
+		
+		if (empty($organizador)) { 
+			$filaErrores['organizador'][] = 'error_Organizador'; 
+		} 
+
+		if(!empty($filaErrores)){
+			$errores[] = $filaErrores;
+			continue;
+		}
+
 
 		$sql = "SELECT id FROM organizadores WHERE nombre = '$organizador'";
 		$result = $conn->query($sql);
@@ -34,6 +79,8 @@ if( fopen("C:\wamp64\www\carpeta\EventosDeportivosCrud\dbExcel.csv","r")) {
 			$sql = "INSERT INTO eventos (nombre_evento, tipo_deporte, fecha, hora, ubicacion, id_organizador) VALUES ('$evento','$tipoDeporte','$fecha', '$hora','$ubicacion','$idOrganizador')";
 
 			if($conn->query($sql)) {
+				$contadorExito ++;
+
 				echo"Se a guadado en la bd";
 
 			}else{
@@ -44,6 +91,20 @@ if( fopen("C:\wamp64\www\carpeta\EventosDeportivosCrud\dbExcel.csv","r")) {
 			echo "No se a encontrado ningun organizador";
 		}
   }
+
+	echo "Valores procesados: $contador .\n";
+	echo "Valores procesados con exito: $contadorExito .\n";
+	echo "Valores no procesados con exito:" . count($errores) .".\n";
+
+	foreach ($errores as $index => $filaErrores) { 
+		$erroresFila = []; 
+		foreach ($filaErrores as $campo => $contenido) { 
+			foreach ($contenido as $error) { $erroresFila[] = "$campo: $error"; }
+		} 
+		echo $index . ": " . implode("; ", $erroresFila) . ".\n";
+	}
+
+
 } else {
 
 	echo "No se a encontrado ningun archivo";
